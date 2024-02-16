9cd2372e7653e8b21128ab2d56f502b22c19ed9b
abhiSolankii <abhisheksolanki01010@gmail.com>
2024-02-16 19:27:41 +0530

    updated date

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   const [lang, setlang] = useState("en");
 
   useEffect(() => {
-    const URL = `https://newsapi.org/v2/everything?q=${query}&from=2023-12-13&language=${lang}&sortBy=popularity&apiKey=317f41ebc2d7416380140d6192f2c857`;
+    const URL = `https://newsapi.org/v2/everything?q=${query}&from=2024-02-15&language=${lang}&sortBy=popularity&apiKey=317f41ebc2d7416380140d6192f2c857`;
     fetch(URL)
       .then((response) => response.json())
       .then((news) => {
